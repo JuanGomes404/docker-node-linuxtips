a96fd9cdbd5fee7874e1742774ceb369889e9b8c
Juan Gomes <67767706+JuanGomes404@users.noreply.github.com>
2023-07-05 19:18:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('juanzinho gameplays e etc!');
+  res.send('cerveja gelada');
 });
 
 app.get('*', (req, res) => {
