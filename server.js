120fac656c038a71da9d3fd13cc2c1822b53d9cc
Juan Gomes <67767706+JuanGomes404@users.noreply.github.com>
2023-07-26 09:55:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('sem querer ser ansioso mas ja pode aomossar');
+  res.send('rodou');
 });
 
 app.get('*', (req, res) => {
