7510b59300dd978f0d36ad1b2809a786ee21060b
Juan Gomes <juangsantanna@gmail.com>
2023-07-26 14:30:47 -0300

    trocando nome de clusters

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('rodou');
+  res.send("Ta rodando, meu querido");
 });
 
 app.get('*', (req, res) => {
-  res.send('oieeeee!!!!!', 404);
+  res.send("não achou nenhuma parada nessa aplicação", 404);
 });
 
 app.listen(PORT, HOST);
